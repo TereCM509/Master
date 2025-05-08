39d22277e45c02603b4d362054593224eb364b4a
Teresa CM <terecortesmag@outlook.com>
2025-05-08 15:07:35 -0600

    Se agrego la funciones y estructuras

---
+++ Test.c
@@ -0,0 +1,31 @@
+/***************************************************
+ESTRUCTURA DE UN PROGRAMA EN C
+Ing. Teresa Cortes Magallon
+11/12/23
+****************************************************/
+
+//Directivas del preprocesador
+#include "stdio.h" 
+#define PI 3.1416     //Macro
+                  
+ struct Cuadrado //Estructuras
+ { int Base;
+   int Altura;
+ };         
+  
+ void FuncionSec(); //Prototipos de funcion
+ float varGlobal=5;//Variables globales
+                  
+int main()   //Funcion principal main
+{   char  Nombre[]="Teresa";   //Declaracion de variables locales
+    
+    printf("Hola %s \n", Nombre);  //Sentencias de codido
+   
+    return 0; //Para indicar al compilador que todo termino bien.
+}
+
+void FuncionSec()   //Funcion
+{ int a=2, b=3; 
+  printf("%d", a+b);
+  return;
+}
